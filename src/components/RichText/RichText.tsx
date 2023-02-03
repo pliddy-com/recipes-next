a2b0e886a8058bc4cae9b8d94b984d80228ebba9
Patrick Liddy <pjliddy@gmail.com>
2023-02-03 11:40:59 -0500

    024-husky set up to run pre-commit scripts
    * npm run typegen
    * npm run typecheck
    * npm run lint

--- src/components/RichText/RichText.tsx
+++ src/components/RichText/RichText.tsx
@@ -88,6 +88,7 @@ const renderOptions = ({ links }: RenderOptionProps): Options => {
           data: { uri },
         } = node ?? {};
 
+        console.log({ children });
         return (
           <Link href={uri} target="_blank" rel="noreferrer">
             {children}
