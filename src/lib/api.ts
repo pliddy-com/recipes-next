c4c500548d1fc6087d01d61bc004e0b468afb391
Patrick Liddy <pjliddy@gmail.com>
2023-02-11 16:01:02 -0500

    041-api-test moved fetch & gql to clients.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,4 @@
-import { createClient, TypedDocumentNode } from 'urql';
-import { print } from 'graphql';
-
+import { fetchContent, queryGraphQLContent } from './clients';
 import {
   RecipeCollectionDocument,
   RecipeCollectionQueryVariables,
@@ -17,75 +15,6 @@ import {
 } from 'types/generated/graphql';
 
 import { notNullOrUndefined } from 'lib/typeUtils';
-import { ResultOf, VariablesOf } from '@graphql-typed-document-node/core';
-
-const SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-const ACCESS_TOKEN = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
-
-// TODO: move whole string to .env
-const API_ENDPOINT = `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/`;
-
-const graphQLRequestClient = createClient({
-  url: API_ENDPOINT,
-  fetchOptions: () => ({
-    headers: { authorization: ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : '' },
-  }),
-});
-
-export async function graphQLRequest<
-  TDocument extends TypedDocumentNode<
-    ResultOf<TDocument>,
-    VariablesOf<TDocument>
-  >,
-  TVars = TDocument extends unknown ? never : VariablesOf<TDocument>
->(document: TDocument, variables: TVars) {
-  if (typeof window !== 'undefined') {
-    throw new Error(
-      'This function in only used for Server Side Rendering. Use `fetch` for client side requests.'
-    );
-  }
-
-  const { data, error } = await graphQLRequestClient
-    // @ts-expect-error query variables are typed by graphQLRequest
-    .query(document, variables)
-    .toPromise();
-
-  if (!data) {
-    if (error) {
-      throw error;
-    }
-    throw new Error('No data returned');
-  }
-
-  return data;
-}
-
-// use fetch for nav query instead of the gql client to minimize browser bundle size
-export async function fetchContent<
-  TDocument extends TypedDocumentNode<
-    ResultOf<TDocument>,
-    VariablesOf<TDocument>
-  >,
-  TVars = TDocument extends unknown ? never : VariablesOf<TDocument>
->(document: TDocument, variables: TVars) {
-  try {
-    const response = await window.fetch(API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : '',
-      },
-      // send the GraphQL query
-      body: JSON.stringify({ query: print(document), variables }),
-    });
-
-    const { data } = await response.json();
-
-    return data;
-  } catch (e) {
-    console.error(e);
-  }
-}
 
 // used to query content for nav menu taxonomy
 export const fetchNavContent = async (
@@ -101,11 +30,13 @@ export const fetchNavContent = async (
     : [];
 };
 
+// GRAPHQL requests
+
 // used to query content for  home index page
 export const queryRecipeCollectionContent = async (
   variables?: RecipeCollectionQueryVariables
 ) => {
-  const { recipeCollection } = await graphQLRequest(
+  const { recipeCollection } = await queryGraphQLContent(
     RecipeCollectionDocument,
     variables
   );
@@ -119,7 +50,7 @@ export const queryRecipeCollectionContent = async (
 export const queryListPageContent = async (
   variables: ListPageQueryQueryVariables
 ) => {
-  const { tagCollection } = await graphQLRequest(
+  const { tagCollection } = await queryGraphQLContent(
     ListPageQueryDocument,
     variables
   );
@@ -131,7 +62,7 @@ export const queryListPageContent = async (
 export const queryRecipeContent = async (
   variables?: RecipePageQueryVariables
 ) => {
-  const { recipeCollection } = await graphQLRequest(
+  const { recipeCollection } = await queryGraphQLContent(
     RecipePageDocument,
     variables
   );
@@ -145,7 +76,7 @@ export const queryRecipeContent = async (
 export const queryPageSlugs = async (
   variables: RecipeSlugsCollectionQueryVariables
 ) => {
-  const { recipeCollection } = await graphQLRequest(
+  const { recipeCollection } = await queryGraphQLContent(
     RecipeSlugsCollectionDocument,
     variables
   );
@@ -162,7 +93,7 @@ export const queryPageSlugs = async (
 export const queryCategorySlugs = async (
   variables: TaxonomyCollectionQueryVariables
 ) => {
-  const { taxonomyCollection } = await graphQLRequest(
+  const { taxonomyCollection } = await queryGraphQLContent(
     TaxonomyCollectionDocument,
     variables
   );
@@ -194,7 +125,7 @@ export const queryCategorySlugs = async (
 export const queryTagSlugs = async (
   variables: TagSlugsCollectionQueryVariables
 ) => {
-  const { tagCollection } = await graphQLRequest(
+  const { tagCollection } = await queryGraphQLContent(
     TagSlugsCollectionDocument,
     variables
   );
---
+++ src/lib/clients.ts
@@ -0,0 +1,62 @@
+import { TypedDocumentNode, createClient } from 'urql';
+import { print } from 'graphql';
+import { ResultOf, VariablesOf } from '@graphql-typed-document-node/core';
+
+const ACCESS_TOKEN = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT || '';
+
+// use fetch for nav query instead of the gql client to minimize browser bundle size
+export async function fetchContent<
+  TDocument extends TypedDocumentNode<
+    ResultOf<TDocument>,
+    VariablesOf<TDocument>
+  >,
+  TVars = TDocument extends unknown ? never : VariablesOf<TDocument>
+>(document: TDocument, variables: TVars) {
+  try {
+    const response = await window.fetch(API_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : '',
+      },
+      // send the GraphQL query
+      body: JSON.stringify({ query: print(document), variables }),
+    });
+
+    const { data } = await response.json();
+
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const gqlClient = createClient({
+  url: API_ENDPOINT,
+  fetchOptions: () => ({
+    headers: { authorization: ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : '' },
+  }),
+});
+
+export async function queryGraphQLContent<
+  TDocument extends TypedDocumentNode<
+    ResultOf<TDocument>,
+    VariablesOf<TDocument>
+  >,
+  TVars = TDocument extends unknown ? never : VariablesOf<TDocument>
+>(document: TDocument, variables: TVars) {
+  const { data, error } = await gqlClient
+    // @ts-expect-error query variables are typed by graphQLRequest
+    .query(document, variables)
+    .toPromise();
+
+  if (!data) {
+    if (error) {
+      throw error;
+    }
+    throw new Error('No data returned');
+  }
+
+  return data;
+}
