de9a809d124c49cfed6973bef32650f72bdd9ee0
Patrick Liddy <pjliddy@gmail.com>
2023-02-19 13:57:23 -0500

    057-layout-test cleaned up test file

--- src/layout/Layout/Layout.test.tsx
+++ src/layout/Layout/Layout.test.tsx
@@ -1,28 +1,23 @@
-// import testing-library methods
-import { render, waitFor } from '@testing-library/react';
-
-// add custom jest matchers from jest-dom
 import '@testing-library/jest-dom';
-// import React from 'react';
-// import the component to test
+import { render, waitFor } from '@testing-library/react';
 import Layout from './Layout';
 import * as api from 'lib/api';
-// import { getNavTaxonomy } from 'lib/api';
 
 jest.mock('lib/api');
 
 describe('Layout', () => {
+  beforeEach(() => {
+    const setState = jest.fn();
+    jest.mock('react', () => ({
+      useEffect: () => {
+        () => jest.fn();
+      },
+      useState: (initial: unknown) => [initial, setState],
+    }));
+  });
+
   describe('when there is page content', () => {
     it('it renders the Layout', async () => {
-      const setState = jest.fn();
-
-      jest.mock('react', () => ({
-        useEffect: () => {
-          () => jest.fn();
-        },
-        useState: (initial: unknown) => [initial, setState],
-      }));
-
       const children = (
         <div key="1">
           <span>test</span>
@@ -43,15 +38,6 @@ describe('Layout', () => {
     });
 
     it('it handles the error', async () => {
-      const setState = jest.fn();
-
-      jest.mock('react', () => ({
-        useEffect: () => {
-          () => jest.fn();
-        },
-        useState: (initial: unknown) => [initial, setState],
-      }));
-
       const spy = jest
         .spyOn(api, 'getNavTaxonomy')
         .mockRejectedValueOnce(new Error());
