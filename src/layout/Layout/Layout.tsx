a428993b9c2c99cb47b1b9889a4465bd478b0a4b
Patrick Liddy <pjliddy@gmail.com>
2023-02-26 14:42:01 -0500

    071-layout cleaned up commented code

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -7,11 +7,6 @@ import NavBar from 'components/Navigation/NavBar/NavBar';
 import { getNavTaxonomy } from 'lib/api';
 
 import { Taxonomy } from 'types/queries';
-// import { useTheme } from '@mui/material/styles';
-// import useMediaQuery from '@mui/material/useMediaQuery';
-// import PayloadRender from 'components/PayloadRender/PayloadRender';
-
-// import baseTheme from 'theme/base';
 
 interface LayoutProps {
   children?: ReactElement[] | ReactElement;
@@ -19,18 +14,6 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   const [navData, setNavData] = useState<Taxonomy>();
-
-  // const theme = useTheme();
-
-  // const breakpoints = {
-  //   xs: useMediaQuery(theme.breakpoints.only('xs')),
-  //   sm: useMediaQuery(theme.breakpoints.only('sm')),
-  //   md: useMediaQuery(theme.breakpoints.only('md')),
-  //   lg: useMediaQuery(theme.breakpoints.only('lg')),
-  //   xl: useMediaQuery(theme.breakpoints.only('xl')),
-  // };
-
-  // const { breakpoints: values } = baseTheme ?? {};
 
   useEffect(() => {
     getNavTaxonomy({ where: { slug: 'categories' } })
@@ -49,8 +32,6 @@ const Layout = ({ children }: LayoutProps) => {
       <NavBar nav={navData} />
       <main>
         <Container className="page" data-testd="page" maxWidth="xl">
-          {/* <PayloadRender payload={breakpoints} />
-          <PayloadRender payload={values as object} /> */}
           {children}
         </Container>
       </main>
