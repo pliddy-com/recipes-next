fe53945a64a0910178f71f5a3905d998650376f8
Patrick Liddy <pjliddy@gmail.com>
2023-03-11 22:05:49 -0500

    086-iac added cdk directory to .github

---
+++ .github/cdk/bin/recipes-aws.ts
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import * as branchName from 'current-git-branch';
+
+import { RecipesSharedStack } from '../lib/recipes-shared-stack';
+import { RecipesBranchStack } from '../lib/recipes-branch-stack';
+
+const { CDK_DEFAULT_ACCOUNT, CDK_DEFAULT_REGION, DOMAIN } = process.env;
+
+const app = new cdk.App();
+const scope = app.node.tryGetContext('scope');
+
+// scope should be 'shared' or 'branch'
+if (!scope)
+  throw 'scope must be provided as a context variable: cdk deploy --context scope={scope} --context branch={branch}';
+if (!['shared', 'branch'].includes(scope))
+  throw 'scope must be "shared" or "branch"';
+
+// git branch if passed in as an optional parameter (to be used by GitHub Actions)
+const paramBranch = app.node.tryGetContext('branch');
+
+// git branch retrieved from current environment
+const gitBranch = branchName();
+
+// use the branch parameter if it was passed in
+const branch = paramBranch || gitBranch;
+
+if (!branch) throw 'git branch name is not available';
+
+// capitalized version of branch for CloudFormation output variables
+const branchLabel = branch && branch[0].toUpperCase() + branch.slice(1);
+
+if (scope === 'shared') {
+  new RecipesSharedStack(
+    app,
+    `RecipesSharedStack${branch === 'main' ? 'Prod' : 'Dev'}`,
+    {
+      domain: DOMAIN ?? 'pliddy.com',
+      subdomain: 'recipes',
+      branch,
+      env: {
+        account: CDK_DEFAULT_ACCOUNT,
+        region: CDK_DEFAULT_REGION,
+      },
+    }
+  );
+} else {
+  new RecipesBranchStack(
+    app,
+    `RecipesBranchStack${branch === 'main' ? 'Prod' : branchLabel}`,
+    {
+      domain: DOMAIN ?? 'pliddy.com',
+      subdomain: 'recipes',
+      branch,
+      env: {
+        account: CDK_DEFAULT_ACCOUNT,
+        region: CDK_DEFAULT_REGION,
+      },
+    }
+  );
+}
+
+/** 
+ *  Deploy a shared stack: 
+ *      cdk deploy --context scope=shared
+ *      npm run deploy:shared
+ * 
+ *  Deploy a branch-specific stack: 
+ *      cdk deploy --context scope=dev
+ *      npm run deploy
+ * 
+ *  Test
+ *      cdk synth --context scope=branch
+ * 
+ * 
+ *  npx cdk \
+        --require-approval never \
+        --verbose \
+        --execute true \
+        --force \
+        --toolkit-stack-name "cdk-toolkit-master" \
+        --app "ts-node ./bin/recipes-aws.ts" \
+        deploy
+ */
---
+++ .github/cdk/lib/recipes-branch-stack.ts
@@ -0,0 +1,220 @@
+#!/usr/bin/env node
+
+/**
+ *  Import functions from aws-cdk-lib
+ */
+
+import { App, Duration, Fn, Stack, StackProps } from 'aws-cdk-lib';
+
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+
+import {
+  AllowedMethods,
+  Distribution,
+  DistributionProps,
+  ErrorResponse,
+  HttpVersion,
+  OriginAccessIdentity,
+  OriginRequestPolicy,
+  PriceClass,
+  ResponseHeadersPolicy,
+  SecurityPolicyProtocol,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
+/**
+ *  define the TypeScript interface for the stack
+ */
+
+export interface RecipesBranchStackProps extends StackProps {
+  domain: string;
+  subdomain: string;
+  branch: string;
+}
+
+/**
+ *  Generate a CloudFormation Stack to deploy site infrastructure:
+ *    - CloudFront distribution
+ *    - Route 53 DNS records for {branch}.recipes.pliddy.com
+ */
+
+export class RecipesBranchStack extends Stack {
+  constructor(scope: App, id: string, props: RecipesBranchStackProps) {
+    super(scope, id, props);
+
+    /*
+     *  Desctructure props
+     */
+
+    const { domain, subdomain, branch, env } = props;
+    const { account, region } = env ?? {};
+
+    if (!account || !region)
+      throw 'Missing environment variable for account and region';
+
+    /**
+     *  import shared AWS resources
+     */
+
+    const siteBucketArn = Fn.importValue(
+      `Recipes-BucketArn-${branch === 'main' ? 'Prod' : 'Dev'}`
+    );
+    const certificateArn = Fn.importValue(
+      `Recipes-Certificate-${branch === 'main' ? 'Prod' : 'Dev'}`
+    );
+    const cloudfrontOAIId = Fn.importValue(
+      `Recipes-OAI-${branch === 'main' ? 'Prod' : 'Dev'}`
+    );
+    const responseHeadersPolicyId = Fn.importValue(
+      `Recipes-ResponseHeadersPolicy-${branch === 'main' ? 'Prod' : 'Dev'}`
+    );
+
+    const siteBucket = Bucket.fromBucketArn(
+      this,
+      'DomainBucket',
+      siteBucketArn
+    );
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      'DomainCertificate',
+      certificateArn
+    );
+    const cloudfrontOAI = OriginAccessIdentity.fromOriginAccessIdentityId(
+      this,
+      'DomainOAI',
+      cloudfrontOAIId
+    );
+    const responseHeadersPolicy =
+      ResponseHeadersPolicy.fromResponseHeadersPolicyId(
+        this,
+        'ResponseHeadersPolicy',
+        responseHeadersPolicyId
+      );
+
+    /**
+     *  Create strings based on branch, subdomain, and domain for use by the stack
+     */
+
+    // capitalize first letter for PascalCase
+    const branchLabel = branch && branch[0].toUpperCase() + branch.slice(1);
+
+    // subdomain used for recipes.pliddy.com
+    const branchSubdomain = `${subdomain}.${domain}`;
+
+    // S3 path for serving branch files
+    const originPath = `/branches/${branch}`;
+
+    // full domain for feature branch: branch.recipes.pliddy.com
+    const siteDomain = `${branch}.${branchSubdomain}`;
+
+    /**
+     *  Identify the Route 53 hosted zone for the domain
+     *
+     *  Generate a CloudFormation output value for the siteUrl for the current branch
+     */
+
+    const hostedZone = HostedZone.fromLookup(this, 'Zone', {
+      domainName: domain,
+    });
+
+    this.exportValue(
+      `https://${branch === 'main' ? branchSubdomain : siteDomain}`,
+      { name: `Recipes-SiteUrl-${branchLabel}` }
+    );
+
+    /**
+     *  Create custom error responses for the CloudFront distribution
+     */
+
+    const customErrorResponse400: ErrorResponse = {
+      httpStatus: 400,
+      ttl: Duration.minutes(10),
+      responseHttpStatus: 400,
+      responsePagePath: '/404.html',
+    };
+
+    const customErrorResponse403: ErrorResponse = {
+      httpStatus: 403,
+      ttl: Duration.minutes(10),
+      responseHttpStatus: 403,
+      responsePagePath: '/404.html',
+    };
+
+    const customErrorResponse404: ErrorResponse = {
+      httpStatus: 404,
+      ttl: Duration.minutes(10),
+      responseHttpStatus: 404,
+      responsePagePath: '/404.html',
+    };
+
+    /**
+     *  Create a CloudFront Web Distribution
+     *
+     *  This solution is using Origin Access Identity (OAI) with Distribution
+     *  to address AWS CDK internal conflicts between Origin Access Control (OAC)
+     *  and OAI when trying to use OAC.
+     *
+     *  Generates a CloudFormation output value for the CloudFront distribution id
+     */
+
+    const distributionConfig: DistributionProps = {
+      certificate,
+      defaultBehavior: {
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        compress: false,
+        origin: new S3Origin(siteBucket, {
+          originPath,
+          originShieldEnabled: true,
+          originShieldRegion: process.env.CDK_DEFAULT_REGION,
+          originAccessIdentity: cloudfrontOAI,
+        }),
+        originRequestPolicy: OriginRequestPolicy.CORS_S3_ORIGIN,
+        responseHeadersPolicy,
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      domainNames: [branch === 'main' ? branchSubdomain : siteDomain],
+      errorResponses: [
+        customErrorResponse400,
+        customErrorResponse403,
+        customErrorResponse404,
+      ],
+      httpVersion: HttpVersion.HTTP2_AND_3,
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      priceClass: PriceClass.PRICE_CLASS_100,
+    };
+
+    const distribution = new Distribution(
+      this,
+      'SiteDistribution',
+      distributionConfig
+    );
+
+    this.exportValue(distribution.distributionId, {
+      name: `Recipes-Distribution-${branchLabel}`,
+    });
+
+    /**
+     *  Create a Route53 alias record for the CloudFront distribution
+     *
+     *  Generates a CloudFormation output value for the Site Alias Record
+     */
+
+    const recordName = branch === 'main' ? branchSubdomain : siteDomain;
+
+    new ARecord(this, `BranchAliasRecord`, {
+      recordName,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      zone: hostedZone,
+    });
+
+    this.exportValue(recordName, {
+      name: `Recipes-SubdomainAliasRecord-${branchLabel}`,
+    });
+  }
+}
