3747b3feb527d6136eb7413fdc83deda64436805
Patrick Liddy <pjliddy@gmail.com>
2023-03-15 19:44:28 -0400

    097-edge handle extra slash on uri

--- .github/cdk/lib/recipes-branch-stack.originRequest.ts
+++ .github/cdk/lib/recipes-branch-stack.originRequest.ts
@@ -14,22 +14,22 @@ exports.handler = async (event: { Records: any[] }): Promise<Handler> => {
   }
 
   // handle /[slug] dynamic route
-  if (requestUri !== '/' && requestUri.startsWith('/')) {
-    request.uri = requestUri + '.html';
-    console.log(
-      'request is a [slug] path, add.html - request.uri:',
-      request.uri
-    );
-    return request;
-  }
+  // if (requestUri !== '/' && requestUri.startsWith('/')) {
+  //   request.uri = requestUri + '.html';
+  //   console.log(
+  //     'request is a [slug] path, add.html - request.uri:',
+  //     request.uri
+  //   );
+  //   return request;
+  // }
 
-  console.log('request.uri:', request.uri);
+  // console.log('request.uri:', request.uri);
 
   // if URI ends with "/" slash, then remove it before appending .html
   if (requestUri.endsWith('/')) {
     request.uri = requestUri.substring(0, requestUri.length - 1);
     console.log(
-      'request ends with a slash, add.html - request.uri:',
+      'request ends with a slash, remove slash - request.uri:',
       request.uri
     );
   }
