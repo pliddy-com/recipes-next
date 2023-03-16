e42c922f0847d718d02250ce0c841978744d85a0
Patrick Liddy <pjliddy@gmail.com>
2023-03-15 21:21:27 -0400

    097-edge revert originRequest to original

--- .github/cdk/lib/recipes-branch-stack.originRequest.ts
+++ .github/cdk/lib/recipes-branch-stack.originRequest.ts
@@ -1,6 +1,8 @@
-import { Handler } from 'aws-cdk-lib/aws-lambda';
+'use strict';
 
-exports.handler = async (event: { Records: any[] }): Promise<Handler> => {
+// this is the origin request lambda@edge for the cloudfront distribution
+
+exports.handler = async (event) => {
   const eventRecord = event.Records[0];
   const request = eventRecord.cf.request;
   const requestUri = request.uri;
@@ -16,20 +18,16 @@ exports.handler = async (event: { Records: any[] }): Promise<Handler> => {
   // handle /[slug] dynamic route
   if (requestUri !== '/' && requestUri.startsWith('/')) {
     request.uri = requestUri + '.html';
-    console.log(
-      'request is a [slug] path, add.html - request.uri:',
-      request.uri
-    );
+    console.log('request is a [slug] path, request.uri:', request.uri);
     return request;
   }
+
+  console.log('request.uri:', request.uri);
 
   // if URI ends with "/" slash, then remove it before appending .html
   if (requestUri.endsWith('/')) {
     request.uri = requestUri.substring(0, requestUri.length - 1);
-    console.log(
-      'request ends with a slash, add "index" - request.uri:',
-      request.uri
-    );
+    console.log('request ends with a slash, request.uri:', request.uri);
   }
 
   request.uri += '.html';
