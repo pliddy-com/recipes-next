292697500af4fe99d5766385a6220fb2054f444f
Patrick Liddy <pjliddy@gmail.com>
2023-03-16 13:39:57 -0400

    098-rewrites map all to 'tag'; eliminated rewrites

--- src/components/Navigation/NavBar/NavBar.test.tsx
+++ src/components/Navigation/NavBar/NavBar.test.tsx
@@ -20,12 +20,8 @@ describe('NavBar', () => {
       value: jest.fn().mockImplementation((query) => ({
         matches: false,
         media: query,
-        // onchange: null,
         addListener: jest.fn(), // Deprecated
         removeListener: jest.fn(), // Deprecated
-        // addEventListener: jest.fn(),
-        // removeEventListener: jest.fn(),
-        // dispatchEvent: jest.fn(),
       })),
     });
   });
