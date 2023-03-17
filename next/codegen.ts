b3310c0c2bde1531acb5c79d51e77305efa2dd55
Patrick Liddy <pjliddy@gmail.com>
2023-03-17 16:23:28 -0400

    099-monorepo added typegen:local to use dotenv

---
+++ next/codegen.ts
@@ -0,0 +1,29 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const {
+  NEXT_PUBLIC_CONTENTFUL_SPACE_ID: spaceId,
+  NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: token
+} = process.env;
+
+const schemaUrl = `https://graphql.contentful.com/content/v1/spaces/${spaceId}/`;
+
+const config: CodegenConfig = {
+  require: ['ts-node/register'],
+  schema: [
+    {
+      [schemaUrl]: {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    }
+  ],
+  documents: ['src/**/*.graphql'],
+  generates: {
+    'src/types/queries.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typed-document-node']
+    }
+  }
+};
+
+export default config;
